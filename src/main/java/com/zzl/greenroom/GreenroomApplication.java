0fb42ddb40f972a0f76f1ac4395402b9f81b8c0d
Administrator <Administrator@2013-20150811DR>
2019-08-20 17:22:47 +0800

    修改

--- src/main/java/com/zzl/greenroom/GreenroomApplication.java
+++ src/main/java/com/zzl/greenroom/GreenroomApplication.java
@@ -1,11 +1,21 @@
 package com.zzl.greenroom;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+
+
+
+@SpringBootApplication()
+@MapperScan("com.zzl.greenroom.module.system.dao")
 public class GreenroomApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GreenroomApplication.class, args);
 	}
